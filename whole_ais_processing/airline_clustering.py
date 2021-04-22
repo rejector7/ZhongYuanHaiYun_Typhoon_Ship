4da6a791dfce25f2eb58c2c30c35c31b80fac8ee
rejector7 <2643719428@qq.com>
2021-04-22 15:33:09 +0800

    ports show by folium

--- whole_ais_processing/airline_clustering.py
+++ whole_ais_processing/airline_clustering.py
@@ -1,14 +1,27 @@
 import pandas as pd
+import folium
+import matplotlib.pyplot as plt
+
+"""
+dim_port file processing and show
+"""
+
+def get_ports():
+    port_file = "../dataset/DIM_PORT.csv"
+    port_dset = pd.read_csv(port_file)
+    # print(port_dset.head())
+    # port_dset = port_dset.sort_values(by='port_id')
+    # print(port_dset.head())
+    # print(port_dset.describe())
+    port_dset = port_dset[['port_id', 'port_cd', 'country_cd', 'latitude', 'longitude']]
+    # print(port_dset.columns)
+    return port_dset
 
 
-port_file = "G:/Reins/projects/中远海运/数据集/dset/DIM_PORT.csv"
+def plot_ports():
+    port_dset = get_ports()
+    fm = folium.Map(location=[25, 120])
+    fm.save("../processed_dset/ports.html")
+    plt.show()
 
-port_dset = pd.read_csv(port_file)
-# print(port_dset.head())
-# port_dset = port_dset.sort_values(by='port_id')
-# print(port_dset.head())
-# print(port_dset.describe())
-
-port_dset = port_dset[['port_id', 'latitude', 'longitude']]
-
-# print(port_dset.columns)
+plot_ports()
